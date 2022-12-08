007bebbd09b6424478978bf91ef90d8e264e991e
HIANDIC <leaands25@gmail.com>
2022-12-08 12:53:58 +0100

    outputs added

--- main.tf
+++ main.tf
@@ -264,3 +264,27 @@ resource "aws_iam_instance_profile" "ec2ecr-profile" {
   name = "swarmprofileproje204"
   role = aws_iam_role.ec2fulltoecr.name
 }
+
+output "leader-manager-public-ip" {
+  value = aws_instance.docker-machine-leader-manager.public_ip
+}
+
+output "website-url" {
+  value = "http://${aws_instance.docker-machine-leader-manager.public_ip}"
+}
+
+output "viz-url" {
+  value = "http://${aws_instance.docker-machine-leader-manager.public_ip}:8080"
+}
+
+output "manager-public-ip" {
+  value = aws_instance.docker-machine-managers.*.public_ip
+}
+
+output "worker-public-ip" {
+  value = aws_instance.docker-machine-workers.*.public_ip
+}
+
+output "ecr-repo-url" {
+  value = aws_ecr_repository.ecr-repo.repository_url
+}
